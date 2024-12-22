103888b4de7fd035989c7e6049d95216657633bf
Susanta Ray <susantaray786@gmail.com>
2024-12-23 01:51:13 +0530

    user, captain login and signup page now ready✔

--- frontend/src/pages/CaptainSignup.jsx
+++ frontend/src/pages/CaptainSignup.jsx
@@ -1,9 +1,102 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 const CaptainSignup = () => {
-  return (
-    <div>CaptainSignup</div>
-  )
-}
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [userData, setUserData] = useState({});
 
-export default CaptainSignup
+  const submitHandler = (e) => {
+    e.preventDefault();
+    setUserData({
+      username: {
+        firstName: firstName,
+        lastName: lastName,
+      },
+      email: email,
+      password: password,
+    });
+    console.log(userData);
+    setEmail("");
+    setPassword("");
+    setFirstName("");
+    setLastName("");
+  };
+  return (
+    <div className="h-screen py-5 px-5 flex flex-col justify-between">
+      <div>
+        <img
+          className="w-[20vw] object-center rounded-[2vw] ml-4 mb-3"
+          src="https://media.istockphoto.com/id/1153445663/vector/steering-wheel-icon-car-and-drive-or-driver-symbol-vector-illustration.jpg?s=612x612&w=0&k=20&c=H58CVFRXlUSp6fT9pjpg-NG4hN-OhJY1xp9rSSrLNBs="
+          alt=""
+        />
+        <form onSubmit={(e) => submitHandler(e)}>
+          <h3 className="text-lg font-medium mb-2">What's your name?</h3>
+
+          <div className="flex gap-4 mb-6">
+            <input
+              required
+              className="bg-[#eeeeee] w-1/2 rounded px-4 py-2 border text-lg placeholder:text-base"
+              type="text"
+              placeholder="first name"
+              value={firstName}
+              onChange={(e) => setFirstName(e.target.value)}
+            />
+            <input
+              required
+              className="bg-[#eeeeee] w-1/2 rounded px-4 py-2 border text-lg placeholder:text-base"
+              type="text"
+              placeholder="last name"
+              value={lastName}
+              onChange={(e) => setLastName(e.target.value)}
+            />
+          </div>
+
+          <h3 className="text-lg font-medium mb-2">What's your email?</h3>
+
+          <input
+            required
+            className="bg-[#eeeeee] mb-6 rounded px-4 py-2 border w-full text-lg placeholder:text-base"
+            type="email"
+            placeholder="email@example.com"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+
+          <h3 className="text-lg font-medium mb-2">Enter Password</h3>
+
+          <input
+            required
+            className="bg-[#eeeeee] mb-6 rounded px-4 py-2 border w-full text-lg placeholder:text-base"
+            type="password"
+            placeholder="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+
+          <button className="bg-[#111] text-white font-semibold mb-3 rounded px-4 py-2 w-full text-lg placeholder:text-base">
+            Register
+          </button>
+
+          <p className="text-center">
+            Already have an account?{" "}
+            <Link to={"/captain-login"} className="text-blue-600">
+              Login here
+            </Link>
+          </p>
+        </form>
+      </div>
+      <div>
+        <p className="text-[10px] leading-tight">
+          By proceeding, you consent to get calls, WhatsApp or SMS messages,
+          including by automated means, from Zippy and its affiliates to the
+          details provided.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default CaptainSignup;
