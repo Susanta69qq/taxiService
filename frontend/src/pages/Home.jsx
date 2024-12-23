e98530ad047ed788c821ab7dfd8d19d098c7ee76
Susanta Ray <susantaray786@gmail.com>
2024-12-23 16:43:21 +0530

    frontend: Creating User UI home page ⚒

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,94 @@
-import React from 'react'
+import React, { useRef, useState } from "react";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+import "remixicon/fonts/remixicon.css";
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
-}
+  const [pickup, setPickup] = useState("");
+  const [destination, setDestination] = useState("");
+  const [panelOpen, setPanelOpen] = useState(false);
+  const panelRef = useRef(null);
+  const panelCloseRef = useRef(null);
 
-export default Home
+  const submitHandler = (e) => {
+    e.preventDefault();
+  };
+
+  useGSAP(() => {
+    if (panelOpen) {
+      gsap.to(panelRef.current, {
+        height: "70%",
+        opacity: 1
+      });
+      gsap.to(panelCloseRef.current, {
+        opacity: 1
+      })
+    } else {
+      gsap.to(panelRef.current, {
+        height: "0%",
+        opacity: 0
+      });
+      gsap.to(panelCloseRef.current, {
+        opacity: 0
+      })
+    }
+  }, [panelOpen]);
+
+  return (
+    <div className="h-screen relative">
+      <img
+        className="w-[16vw] absolute left-5 top-5"
+        src="/logo/zippy.png"
+        alt=""
+      />
+      <div className="h-screen w-screen">
+        {/* image for temporary use */}
+        <img
+          className="h-full w-full object-cover"
+          src="https://miro.medium.com/v2/resize:fit:1400/0*gwMx05pqII5hbfmX.gif"
+          alt=""
+        />
+      </div>
+      <div className="flex flex-col justify-end h-screen absolute top-0 w-full">
+        <h5
+          ref={panelCloseRef}
+          onClick={() => {
+            setPanelOpen(false);
+          }}
+          className="opacity-0"
+        >
+          <i className="ri-arrow-down-wide-line absolute z-[2] right-8 top-8 text-2xl"></i>
+        </h5>
+        <div className="h-[30%] bg-white p-6 relative">
+          <h4 className="text-2xl font-semibold">Find a trip</h4>
+          <form
+            onSubmit={(e) => {
+              submitHandler(e);
+            }}
+          >
+            <div className="line absolute h-16 w-1 top-[45%] left-10 bg-gray-700 rounded-full"></div>
+            <input
+              onClick={() => setPanelOpen(true)}
+              className="bg-[#eeeeee] px-12 py-2 text-lg rounded-lg w-full mt-5"
+              type="text"
+              placeholder="Add a pick-up location"
+              value={pickup}
+              onChange={(e) => setPickup(e.target.value)}
+            />
+            <input
+              onClick={() => setPanelOpen(true)}
+              className="bg-[#eeeeee] px-12 py-2 text-lg rounded-lg w-full mt-3"
+              type="text"
+              placeholder="Enter your destination"
+              value={destination}
+              onChange={(e) => setDestination(e.target.value)}
+            />
+          </form>
+        </div>
+        <div ref={panelRef} className="opacity-0 bg-red-500 h-[0%]"></div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
