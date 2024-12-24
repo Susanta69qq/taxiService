b4c43b72d66727893360990faf8fb1b60c6f9467
Susanta Ray <susantaray786@gmail.com>
2024-12-24 15:33:10 +0530

    frontend: User confirm ride panel ready ✔

---
+++ frontend/src/components/ConfirmedVehiclePanel.jsx
@@ -0,0 +1,56 @@
+import React from "react";
+
+const ConfirmedVehiclePanel = (props) => {
+  console.log(props);
+  return (
+    <div>
+      <h5
+        onClick={() => props.setConfirmVehiclePanel(false)}
+        className="p-1 text-center absolute top-0 w-[93%]"
+      >
+        <i className="ri-arrow-down-wide-line text-3xl text-gray-600"></i>
+      </h5>
+      <h3 className="text-2xl font-semibold mb-5">Confirm your Ride</h3>
+
+      <div className="flex gap-2 flex-col justify-between items-center">
+        <img
+          className="h-20"
+          src="https://www.uber-assets.com/image/upload/f_auto,q_auto:eco,c_fill,h_368,w_552/v1569012915/assets/4f/599c47-7f5c-4544-a5d2-926babc8e113/original/Lux.png"
+          alt=""
+        />
+        <div className="w-full mt-5">
+          <div className="flex items-center gap-5 p-3 border-b-2">
+            <i className="ri-map-pin-user-fill"></i>
+            <div>
+              <h3 className="text-lg font-medium">562/11-A</h3>
+              <p className="text-sm -mt-1 text-gray-600">
+                Kankariya Talab, Bhopal
+              </p>
+            </div>
+          </div>
+          <div className="flex items-center gap-5 p-3 border-b-2">
+            <i className="ri-map-pin-2-fill text-lg"></i>
+            <div>
+              <h3 className="text-lg font-medium">562/11-A</h3>
+              <p className="text-sm -mt-1 text-gray-600">
+                Kankariya Talab, Bhopal
+              </p>
+            </div>
+          </div>
+          <div className="flex items-center gap-5 p-3">
+            <i className="ri-currency-line"></i>
+            <div>
+              <h3 className="text-lg font-medium">₹193.20</h3>
+              <p className="text-sm -mt-1 text-gray-600">Cash Cash</p>
+            </div>
+          </div>
+        </div>
+        <button className="w-full mt-5 bg-green-600 text-white font-semibold p-2 rounded-lg">
+          Confirm
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default ConfirmedVehiclePanel;
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -3,6 +3,8 @@ import gsap from "gsap";
 import { useGSAP } from "@gsap/react";
 import "remixicon/fonts/remixicon.css";
 import LocationPanel from "../components/LocationPanel";
+import VehiclePanel from "../components/VehiclePanel";
+import ConfirmedVehiclePanel from "../components/ConfirmedVehiclePanel";
 
 const Home = () => {
   const [pickup, setPickup] = useState("");
@@ -11,7 +13,9 @@ const Home = () => {
   const panelRef = useRef(null);
   const panelCloseRef = useRef(null);
   const vehiclePanelRef = useRef(null);
+  const confirmVehiclePanelRef = useRef(null)
   const [vehiclePanelOpen, setVehiclePanelOpen] = useState(false);
+  const [confirmVehiclePanel, setConfirmVehiclePanel] = useState(false);
 
   const submitHandler = (e) => {
     e.preventDefault();
@@ -50,6 +54,18 @@ const Home = () => {
       });
     }
   }, [vehiclePanelOpen]);
+
+  useGSAP(() => {
+    if (confirmVehiclePanel) {
+      gsap.to(confirmVehiclePanelRef.current, {
+        transform: "translateY(0)",
+      });
+    } else {
+      gsap.to(confirmVehiclePanelRef.current, {
+        transform: "translateY(100%)",
+      });
+    }
+  }, [confirmVehiclePanel]);
 
   return (
     <div className="h-screen relative overflow-hidden">
@@ -111,75 +127,15 @@ const Home = () => {
       </div>
       <div
         ref={vehiclePanelRef}
-        className="fixed w-full z-10 bottom-0 translate-y-full bg-white px-3 py-10 pt-14"
+        className="fixed w-full z-10 bottom-0 translate-y-full bg-white px-3 py-10 pt-12"
       >
-        <h5
-          onClick={() => setVehiclePanelOpen(false)}
-          className="p-1 text-center absolute top-0 w-[93%]"
-        >
-          <i className="ri-arrow-down-wide-line text-3xl text-gray-600"></i>
-        </h5>
-        <h3 className="text-2xl font-semibold mb-5">Choose a Vehicle</h3>
-        <div className="flex border-2 active:border-black mb-2 rounded-xl w-full p-3 items-center justify-between">
-          <img
-            className="w-20"
-            src="https://www.uber-assets.com/image/upload/f_auto,q_auto:eco,c_fill,h_368,w_552/v1569012915/assets/4f/599c47-7f5c-4544-a5d2-926babc8e113/original/Lux.png"
-            alt=""
-          />
-          <div className="ml-4 w-1/2">
-            <h4 className="font-medium text-base">
-              ZippyGo{" "}
-              <span>
-                <i className="ri-user-3-fill"></i>4
-              </span>
-            </h4>
-            <h5 className="font-medium text-sm">2 mins away</h5>
-            <p className="font-normal text-xs text-gray-600">
-              Affordable, compact rides
-            </p>
-          </div>
-          <h2 className="text-xl font-semibold">₹193.30</h2>
-        </div>
-        <div className="flex border-2 active:border-black mb-2 rounded-xl w-full p-3 items-center justify-between">
-          <img
-            className="w-20"
-            src="https://www.uber-assets.com/image/upload/f_auto,q_auto:eco,c_fill,h_368,w_552/v1649231091/assets/2c/7fa194-c954-49b2-9c6d-a3b8601370f5/original/Uber_Moto_Orange_312x208_pixels_Mobile.png"
-            alt=""
-          />
-          <div className="ml-2 w-1/2">
-            <h4 className="font-medium text-base">
-              ZippyMoto{" "}
-              <span>
-                <i className="ri-user-3-fill"></i>1
-              </span>
-            </h4>
-            <h5 className="font-medium text-sm">3 mins away</h5>
-            <p className="font-normal text-xs text-gray-600">
-              Affordable, Motorcycle ride
-            </p>
-          </div>
-          <h2 className="text-xl font-semibold">₹65.17</h2>
-        </div>
-        <div className="flex border-2 active:border-black mb-2 rounded-xl w-full p-3 items-center justify-between">
-          <img
-            className="w-20"
-            src="https://www.uber-assets.com/image/upload/f_auto,q_auto:eco,c_fill,h_368,w_552/v1648431773/assets/1d/db8c56-0204-4ce4-81ce-56a11a07fe98/original/Uber_Auto_558x372_pixels_Desktop.png"
-            alt=""
-          />
-          <div className="ml-2 w-1/2">
-            <h4 className="font-medium text-base">
-              ZippyAuto{" "}
-              <span>
-                <i className="ri-user-3-fill"></i>3
-              </span>
-            </h4>
-            <h5 className="font-medium text-sm">2 mins away</h5>
-            <p className="font-normal text-xs text-gray-600">
-              Affordable, Auto ride
-            </p>
-          </div>
-          <h2 className="text-xl font-semibold">₹118.68</h2>
-        </div>
+        <VehiclePanel setConfirmVehiclePanel={setConfirmVehiclePanel} setVehiclePanelOpen={setVehiclePanelOpen} />
+      </div>
+      <div
+        ref={confirmVehiclePanelRef}
+        className="fixed w-full z-10 bottom-0 translate-y-full bg-white px-3 py-6 pt-12"
+      >
+        <ConfirmedVehiclePanel setConfirmVehiclePanel={setConfirmVehiclePanel} />
       </div>
     </div>
   );
